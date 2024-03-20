9da9a20d6ef69a8668e0a4a4c4862d1d22939cc0
myung2-love <famousmyung@gmail.com>
2024-03-21 02:25:39 +0900

    Update authorization and Authentication Algorithm

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response, current_app, g
+from flask_cors import CORS
 from Miniter.config import connect_miniter_db as miniter_db
 from json import JSONEncoder
+from datetime import datetime, timedelta
+from functools import wraps
+import bcrypt
+import jwt
+
+
 
 ## Default JSON encoder는 set을 JSON으로 변환할 수 없다.
 ## 그러므로 커스텀 엔코더를 작성해서 set을 list로 변환하여
@@ -10,6 +17,27 @@ class CustomJSONEncoder(JSONEncoder):
         if isinstance(obj, set):
             return list(obj)
         return super().default(self, obj)
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        access_token = request.headers.get('Authorization')
+        if access_token is not None:
+            try:
+                payload = jwt.decode(access_token, current_app.config['JWT_SECRET_KEY'], 'HS256')
+            except jwt.InvalidTokenError:
+                payload = None
+
+            if payload is None: return Response(status=401)
+
+            user_id = payload['user_id']
+            g.user_id = user_id
+            g.user = get_user(user_id) if user_id else None
+        else:
+            return Response(status=401)
+
+        return f(*args, **kwargs)
+    return decorated_function
 
 def get_user(user_id):
     db = miniter_db()
@@ -155,10 +183,28 @@ def get_timeline(user_id):
     
     return timeline
 
+def look_up_email(email):
+    db = miniter_db()
+    try:
+        with db.cursor() as cur:
+            sql = '''
+                SELECT
+                    id,
+                    hashed_password
+                FROM users
+                WHERE email = %(email)s
+            '''
+            cur.execute(sql, {'email':email})
+            row = cur.fetchone()
+    finally:
+        db.close()
+
+    return row if row else None
 
 def create_app(test_config=None):
     app = Flask(__name__)
     app.json_encoder = CustomJSONEncoder
+    CORS(app)
 
     if test_config is None:
         app.config.from_pyfile("config.py")
@@ -172,10 +218,17 @@ def create_app(test_config=None):
     @app.route("/sign-up", methods=['POST'])
     def sign_up():
         new_user = request.json
-        new_user_id = insert_user(new_user)
-        new_user = get_user(new_user_id)
+        email = new_user['email']
+        password = new_user['password']
 
-        return jsonify(new_user)
+        new_user['password'] = bcrypt.hashpw(
+            new_user['password'].encode('UTF-8'),
+            bcrypt.gensalt()
+        )
+        new_user_id = insert_user(new_user)
+        new_user_info = get_user(new_user_id)
+
+        return jsonify(new_user_info)
     
     @app.route('/user/<int:user_id>', methods=['DELETE'])
     def unregister(user_id):
@@ -183,9 +236,33 @@ def create_app(test_config=None):
 
         return '', 200
 
+    @app.route('/login', methods=['POST'])
+    def login():
+        credential = request.json
+        email = credential['email']
+        password = credential['password']
+        user = look_up_email(email)
+
+        if user and bcrypt.checkpw(password.encode('UTF-8'), user['hashed_password'].encode('UTF-8')):
+            user_id = user['id']
+            payload = {
+                'user_id': user_id,
+                'exp'    : datetime.utcnow() + timedelta(seconds = 60*60*24)
+            }
+            token = jwt.encode(payload, app.config['JWT_SECRET_KEY'], 'HS256')
+             
+            return jsonify({
+                'access_token': token
+                })
+        else:
+            return '', 401
+
+    
     @app.route('/tweet', methods=['POST'])
+    @login_required
     def tweet():
         user_tweet = request.json
+        user_tweet['user_id'] = g.user_id
         tweet = user_tweet['tweet']
 
         if len(tweet) > 300:
@@ -196,25 +273,34 @@ def create_app(test_config=None):
         return '', 200
     
     @app.route('/follow', methods=['POST'])
+    @login_required
     def follow():
         payload = request.json
+        payload['id'] = g.user_id
         insert_follow(payload)
 
         return '', 200
     
     @app.route('/unfollow', methods=['POST'])
+    @login_required
     def unfollow():
         payload = request.json
+        payload['id'] = g.user_id
         delete_follow(payload)
 
         return '',200
 
-    @app.route('/timeline/<int:user_id>', methods=['GET'])
-    def timeline(user_id):
+    @app.route('/timeline', methods=['GET'])
+    @login_required
+    def timeline():
+        user_id = g.user_id
+
         return jsonify({
             'user_id'  : user_id,
             'timeline' : get_timeline(user_id)
             })
+
+
     return app
 
 
